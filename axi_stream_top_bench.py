abf4ed37fbe9dff6aac8f26006cb920fc4a1c455
Patrick Seidel <psycowithespn1@gmail.com>
2021-02-20 17:02:53 -0500

    Add a property test to ensure that once tvalid goes high, it stays high until the transmission is done.

--- axi_stream_top_bench.py
+++ axi_stream_top_bench.py
@@ -6,8 +6,19 @@ from axi_stream_top import AxiStreamTop
 dut = AxiStreamTop()
 
 def bench():
-    for _ in range(1000):
+    tvalid_last = 0
+    tvalid_can_fall = 0
+    for i in range(2000):
         yield
+        ### Check that TVALID never goes low until the transmission completes.
+        tvalid = (yield dut.tx.m_axis.tvalid)
+        tx_done = (yield dut.tx.m_axis.tx_done())
+        # Don't care about no change in tvalid.
+        # Don't care if tvalid is high.
+        # Only allow tvalid to fall the cycle after a tx_done.
+        assert((tvalid == tvalid_last) or (tvalid) or (tvalid_can_fall))
+        tvalid_can_fall = tx_done
+        tvalid_last = tvalid
 
 sim = Simulator(dut)
 sim.add_clock(1e-6)
