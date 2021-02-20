f7c3384a44df4f818c90ce0ee2dfe0361eae9bf0
Patrick Seidel <psycowithespn1@gmail.com>
2021-02-20 17:01:06 -0500

    Enable the AxiStreamRx's Lfsr. Count the number of bytes that make it through.

--- axi_stream_rx.py
+++ axi_stream_rx.py
@@ -10,13 +10,18 @@ class AxiStreamRx(Elaboratable):
     def __init__(self):
         self.s_axis = AxiStream(prefix = "s", tuser_bits = 100)
         self.lfsr = Lfsr()
+        self.rx_byte_count = Signal(32)
 
     def elaborate(self, platform):
         m = Module()
 
         m.submodules += self.lfsr
 
+        m.d.comb += self.lfsr.en.eq(1)
         m.d.comb += self.s_axis.tready.eq(self.lfsr.out[0])
+
+        with m.If(self.s_axis.tx_done()):
+            m.d.sync += self.rx_byte_count.eq(self.rx_byte_count + 1)
 
         return m
 
